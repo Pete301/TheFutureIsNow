6b21c44df0de14a8e5c3a4a4dc2bd0db1766e18b
Pete301 <peterjesson301@gmail.com>
2014-07-19 11:13:51 +0100

    Initial Commit

---
+++ Assets/DotSpawner/MoveSpawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveSpawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float y = Random.Range (-5, 5);
+		Vector3 curLoc = gameObject.transform.position;
+		Vector3 location = curLoc;
+
+		location.y = y;
+		gameObject.transform.position = location;
+	}
+}
---
+++ Assets/DotSpawner/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Spawner : MonoBehaviour {
+
+	public Transform dot;
+	public int i;
+	// Use this for initialization
+
+	void Start () {
+		i = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		while (i == 0) 
+		{
+			Instantiate (dot, gameObject.transform.position, Quaternion.identity);
+			i++;
+		}
+	}
+}
